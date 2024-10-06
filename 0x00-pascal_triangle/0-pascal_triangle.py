1e8e224115f723a265337a9c83832731326c37cc
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-06 19:52:50 +0300

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    """ functuion to return pascals triangle """
+    if n <= 0:
+        return []
+
+    pascal = []
+    for i in range(n):
+        row = [1]
+
+        if i > 0:
+            for j in range(1, i):
+                row.append(pascal[i-1][j-1] + pascal[i-1][j])
+            row.append(1)
+        pascal.append(row)
+    return pascal
