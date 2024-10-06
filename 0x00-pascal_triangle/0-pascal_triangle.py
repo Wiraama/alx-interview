0228dbbd5ae1a2366a5c9e23c254e7a9e668cf4c
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-06 19:58:33 +0300

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-""" pascal trianle"""
+"""Pascal's triangle module."""
+
+
 def pascal_triangle(n):
-    """ functuion to return pascals triangle """
+    """Function to return Pascal's triangle of size n.
+
+    Args:
+        n (int): The number of rows in the triangle.
+
+    Returns:
+        list: A list of lists of integers representing Pascal's triangle.
+    """
     if n <= 0:
         return []
 
@@ -11,7 +20,7 @@ def pascal_triangle(n):
 
         if i > 0:
             for j in range(1, i):
-                row.append(pascal[i-1][j-1] + pascal[i-1][j])
+                row.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
             row.append(1)
         pascal.append(row)
     return pascal
