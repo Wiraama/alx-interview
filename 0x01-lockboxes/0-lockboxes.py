0dd410924fdef1de31d346e267dbe505944ac5c2
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-10 18:39:03 +0300

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    unlocked = set([0])
+    keys = [0]
+
+    while keys:
+        box = keys.pop()
+
+        for key in boxes[box]:
+            if key not in unlocked and key < len(boxes):
+                unlocked.add(key)
+                keys.append(key)
+
+    return len(unlocked) == len(boxes)
