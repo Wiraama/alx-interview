e6e2c177e653b945259541bfc190d73590bfc168
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-25 00:54:23 +0300

    matrix rotate

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" rotates 2d matrix 90 degrees """
+
+
+def rotate_2d_matrix(matrix):
+    """ function to rotate
+    """
+    # getting number of rows
+    n = len(matrix)
+    # traversing matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = matrix[i][j]
+
+    for i in range(n):
+        matrix[i].reverse()
