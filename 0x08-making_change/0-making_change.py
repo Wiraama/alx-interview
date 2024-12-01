9249473ff4dfb13290ee4a18dc07576d6dbfc4ce
Wiraama <mawira01@students.uonbi.ac.ke>
2024-12-01 15:10:51 +0300

    task

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" determining fewest number of coins needed to meet given amount """
+
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    count = 0
+    coins.sort(reverse=True)
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            count += 1
+
+            if total == 0:
+                break
+    if total != 0:
+        return -1
+    
+    return count
