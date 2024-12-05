8457e88abd40a4ccbc68ac72942d3a8fb4198733
Wiraama <mawira01@students.uonbi.ac.ke>
2024-12-05 03:10:53 +0300

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -22,10 +22,10 @@ def island_perimeter(grid):
             if grid[i][j] == 1:
                 perimeter += 4 # for the 4 sides
                 
-            if i > 0 and grid[i - 1][j] == 1:
-                perimeter -= 2
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
 
-            if j > 0 and grid[i][j - 1] == 1:
-                perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
 
     return perimeter
